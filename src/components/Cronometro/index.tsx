ae5e21338f06d1f096a41c616849493387797b19
aicha <aichatassa@gmail.com>
2022-03-13 17:14:02 -0300

    useEffects

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -3,16 +3,20 @@ import Relogio from './Relogio';
 import style from './Cronometro.module.scss';
 import { tempoParaSegundos } from '../../common/utils/time';
 import { ITarefa } from '../../types/tarefa';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 interface Props {
     selecionado: ITarefa | undefined
 }
 
 export default function Cronometro({ selecionado }:Props) {
     const [tempo, setTempo] = useState<number>();
-    if(selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
+    
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
