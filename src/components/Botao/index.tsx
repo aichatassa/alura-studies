e9ec67834268f667cc77895835b01f597a5e9a94
aicha <aichatassa@gmail.com>
2022-03-13 16:34:27 -0300

    adding props and states

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component {
+class Botao extends React.Component<{
+    //?: significa que é uma prop opcional
+    type?: "button" | "submit" | "reset" | undefined
+}> {
     render() {
+        //se o tipo não for especificado, será o tipo button
+        const { type = "button" } = this.props; 
         return (
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,10 +1,11 @@
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo:string }){
+export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa){
     return(
         <li className={style.item}>
-            <h3>{tarefa}</h3>
-            <span>{tempo}</span>
+            <h3> {tarefa} </h3>
+            <span> {tempo} </span>
         </li>
     )
 }
